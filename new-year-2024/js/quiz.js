5ef064aded6297e6bcb0d92f62a098a520b67b36
mrgick <mrgickcool@gmail.com>
2023-12-29 19:02:50 +0300

    Add 3 module.

--- new-year-2024/js/quiz.js
+++ new-year-2024/js/quiz.js
@@ -53,36 +53,38 @@ const SONG_QUESTIONS = [
     ["8.mp3", "Пять минут (Карнавальная ночь)"],
     ["9.mp3", "Звенит январская вьюга (Иван Васильевич меняет профессию)"],
     ["10.mp3", "Голубой вагон (Чебурашка)"]
-
 ]
 
-const set_quiz = (question) => {
-    document.getElementsByClassName("quiz")[0].style["display"] = "flex"
-    document.getElementById("quiz-question").innerHTML = (question + 1).toString() + ". " + QUIZ_QUESTIONS[question][0]
-    document.getElementById("quiz-answer").innerHTML = QUIZ_QUESTIONS[question][1]
+const MULTFILM_QUESTIONS = [
+    ["1.jpg", "Двенадцать месяцев"],
+    ["2.jpg", "Ночь перед Рождеством"],
+    ["3.jpg", "Снеговик почтовик"],
+    ["4.jpg", "Щелкунчик"],
+    ["5.jpg", "Снежная королева"],
+    ["6.jpg", "Дед мороз и лето"],
+    ["7.jpg", "Жил был пёс"],
+    ["8.jpg", "Простоквашино"],
+    ["9.jpg", "Дед мороз и серый волк"],
+    ["10.jpg", "Падал прошлогодний снег"]
+]
 
+const set_quiz = (question, QUESTIONS) => {
+    document.getElementsByClassName("quiz")[0].style["display"] = "flex"
+    document.getElementById("quiz-question").innerHTML = (question + 1).toString() + ". " + QUESTIONS[question][0]
+    document.getElementById("quiz-answer").innerHTML = QUESTIONS[question][1]
     document.getElementById("control-answer").onclick = (e) => {
         document.getElementById("quiz-answer").style["display"] = "block"
     }
 
-    document.getElementById("control-next").onclick = (e) => {
-        if (question + 1 < QUIZ_QUESTIONS.length) {
-            window.location = `quiz.html?quiz_type=quiz&question=${question + 1}`
-        } else {
-            window.location = 'index.html'
-        }
-    }
-
-
 }
 
-const set_song = (question) => {
+const set_song = (question, QUESTIONS) => {
     document.getElementsByClassName("song")[0].style["display"] = "flex"
     document.getElementById("song-answer").innerHTML = (question + 1).toString() + ". Откуда отрывок песни?"
-    document.getElementById("music-player").src = `songs/${SONG_QUESTIONS[question][0]}`
+    document.getElementById("music-player").src = `songs/${QUESTIONS[question][0]}`
     document.getElementById("control-answer").onclick = (e) => {
         if (document.getElementById("music-player").currentTime != 0.0) {
-            document.getElementById("song-answer").innerHTML = "Ответ: " + SONG_QUESTIONS[question][1]
+            document.getElementById("song-answer").innerHTML = "Ответ: " + QUESTIONS[question][1]
         }
     }
     document.getElementById("play-pause").onclick = (e) => {
@@ -94,12 +96,14 @@ const set_song = (question) => {
         }
     }
 
-    document.getElementById("control-next").onclick = (e) => {
-        if (question + 1 < SONG_QUESTIONS.length) {
-            window.location = `quiz.html?quiz_type=song&question=${question + 1}`
-        } else {
-            window.location = 'index.html'
-        }
+}
+
+const set_multfilm = (question, QUESTIONS) => {
+    document.getElementsByClassName("multfilm")[0].style["display"] = "block"
+    document.getElementById("multfilm-answer").innerHTML = (question + 1).toString() + ". Откуда картинка?"
+    document.getElementById("multfilm-image").src = `images/${QUESTIONS[question][0]}`
+    document.getElementById("control-answer").onclick = (e) => {
+        document.getElementById("multfilm-answer").innerHTML = QUESTIONS[question][1]
     }
 }
 
@@ -114,18 +118,25 @@ const set_settings = () => {
     _list[1].innerHTML = "Ответ"
     _list[2].innerHTML = "Предыдущий"
     _list[3].innerHTML = "Следующий"
+    document.getElementById("control-answer").onclick = (e) => {
+        _list[1].innerHTML = "ОТВЕТ"
+    }
 }
 
 window.onload = () => {
     let params = new URLSearchParams(document.location.search);
     let question = parseInt(params.get("question"), 10)
     let quiz_type = params.get("quiz_type")
+    let QUESTIONS = []
     if (quiz_type == "quiz") {
-        set_quiz(question)
+        QUESTIONS = QUIZ_QUESTIONS
+        set_quiz(question, QUESTIONS)
     } else if (quiz_type == "song") {
-        set_song(question)
+        QUESTIONS = SONG_QUESTIONS
+        set_song(question, QUESTIONS)
     } else if (quiz_type == "multfilm") {
-
+        QUESTIONS = MULTFILM_QUESTIONS
+        set_multfilm(question, QUESTIONS)
     } else {
         set_settings()
     }
@@ -141,4 +152,11 @@ window.onload = () => {
         window.location = `quiz.html?quiz_type=${quiz_type}&question=${question}`
     }
 
+    document.getElementById("control-next").onclick = (e) => {
+        if (question + 1 < QUESTIONS.length) {
+            window.location = `quiz.html?quiz_type=${quiz_type}&question=${question + 1}`
+        } else {
+            window.location = 'index.html'
+        }
+    }
 }
